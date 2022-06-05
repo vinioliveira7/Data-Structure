2b0c3af7a82b943d9922a519dc7d4e72337bdd26
Vinicius Oliveira <vinicius_oc09@hotmail.com>
2022-06-05 15:49:45 -0300

    remove function

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -5,30 +5,30 @@ typedef struct vertice{
 
     int id;
 
-    struct vertice * dir;
-    struct vertice * esq;
+    struct vertice * right;
+    struct vertice * left;
 
 }VERTICE;
 
-VERTICE *raiz = NULL;
+VERTICE *root = NULL;
 int tam = 0;
 
 VERTICE* search(int id, VERTICE* aux){
     if(aux != NULL){   
         if(aux->id == id){
-            return raiz;
+            return root;
 
         }else if(aux->id > id){
-            if(aux->esq != NULL){
-                return search(id, aux->esq);
+            if(aux->left != NULL){
+                return search(id, aux->left);
 
             }else{
                 return aux;
             }
 
         }else if(aux->id < id){
-            if(aux->dir != NULL){
-                return search(id, aux->dir);
+            if(aux->right != NULL){
+                return search(id, aux->right);
             
             }else{
                 return aux;
@@ -41,10 +41,10 @@ VERTICE* search(int id, VERTICE* aux){
 }
 
 
-void adicionar(int id){
+void insert(int id){
 
     //busca
-    VERTICE *aux = search(id, raiz); 
+    VERTICE *aux = search(id, root); 
     
     //condicao de existencia com base na busca
     if(aux != NULL && aux->id == id){ //se aux->id vinhesse como primeira condição daria estouro de memoria
@@ -53,57 +53,109 @@ void adicionar(int id){
     }else{
         VERTICE* novo = malloc(sizeof(VERTICE));
         novo->id = id;
-        novo->dir = NULL;
-        novo->esq = NULL;
+        novo->right = NULL;
+        novo->left = NULL;
 
         if(aux == NULL){
-            raiz = novo;
+            root = novo;
 
         }else{
             if(aux->id > id){
-                aux->esq = novo;
+                aux->left = novo;
 
             }else{
-                aux->dir = novo;
+                aux->right = novo;
             }
         }
     }
     
 }
 
+VERTICE *removal(int id, VERTICE *aux){
+    
+    //checar se o elemento existe
+    if(aux != NULL){
 
+        //checar se o aux é o elemento escolhido
+        if(aux->id == id){  
+            //remover folha
+            if(aux->left == NULL && aux->right == NULL){
+                free(aux);
+                return NULL;
+
+            //remover com 1 filho
+            }else if(aux->left == NULL || aux->right == NULL){
+                VERTICE *storage;
+
+                //armazena dependendo do ponteiro
+                if(aux->left != NULL){
+                    storage = aux->left;
+                }else{
+                    storage = aux->right;
+                }
+                free(aux);
+                return storage;
+
+            //remover com 2 filhos
+            }else{
+                VERTICE *storage = aux->left;
+                while(storage->right != NULL){
+                    storage = storage->right;
+                }
+                aux->id = storage->id;
+                storage->id = id; 
+                aux->left = removal(id, aux->left);
+                return root;
+            }
+            
+        }else{
+            //caminhar na arvore de forma recursiva e depois retornar a raiz
+            if(aux->id > id){
+                aux->left = removal(id, aux->left);
+
+            }else{
+                aux->right = removal(id, aux->right);
+
+            }
+            return aux;
+        }
+    }else{
+        printf("Valor nao encontrado!\n");
+        return NULL;
+    }
+}
 
 void pre_ordem(VERTICE *aux){
     printf("%d ", aux->id);
 
-    if(aux->esq != NULL){
-        pre_ordem(aux->esq);
+    if(aux->left != NULL){
+        pre_ordem(aux->left);
     }
 
-    if(aux->dir != NULL){
-        pre_ordem(aux->dir);
+    if(aux->right != NULL){
+        pre_ordem(aux->right);
     }
 }
 
 void in_ordem(VERTICE *aux){
-    if(aux->esq != NULL){
-        in_ordem(aux->esq);
+    if(aux->left != NULL){
+        in_ordem(aux->left);
     }
 
     printf("%d ", aux->id);
 
-    if(aux->dir != NULL){
-        in_ordem(aux->dir);
+    if(aux->right != NULL){
+        in_ordem(aux->right);
     }
 }
 
 void pos_ordem(VERTICE *aux){
-    if(aux->esq != NULL){
-        pos_ordem(aux->esq);
+    if(aux->left != NULL){
+        pos_ordem(aux->left);
     }
 
-    if(aux->dir != NULL){
-        pos_ordem(aux->dir);
+    if(aux->right != NULL){
+        pos_ordem(aux->right);
     }
 
     printf("%d ", aux->id);
@@ -112,14 +164,32 @@ void pos_ordem(VERTICE *aux){
 
 
 int main(){
-    adicionar(10);
-    adicionar(5);
-    adicionar(11);
-    adicionar(3);
-    adicionar(8);
+    insert(6);
+    insert(2);
+    insert(8);
+    insert(1);
+    insert(3);
+    insert(9);
+    insert(5);
     
-    in_ordem(raiz);
+    in_ordem(root);
     printf("\n");
+
+    removal(1, root);
+
+    in_ordem(root);
+    printf("\n");
+    
+    removal(3, root);
+
+    in_ordem(root);
+    printf("\n");
+
+    removal(6, root);
+
+    in_ordem(root);
+    printf("\n");
+
     
 
     return 0;
